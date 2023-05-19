594247618befdcb1d40529fb90b06c0b874a906c
VictorTroyAvila <116412427+VictorTroyAvila@users.noreply.github.com>
2023-05-19 23:06:31 +0800

    Unsend

--- Features1/Features1/Program.cs
+++ Features1/Features1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class PUPHubChatsUnsent
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your chat message: ");
+        string chatMessage = Console.ReadLine();
+
+        bool isMessageSent = false;
+
+        Console.WriteLine("Do you Want to unsent the message? (yes/no): ");
+        string userInput = Console.ReadLine().ToLower();
+
+        if (userInput == "yes")
+        {
+            Console.WriteLine("You Unsent a Message");
+            isMessageSent = true;
+        }
+        else if (userInput == "no")
+        {
+            Console.WriteLine("Message Has Been Sent");
+            isMessageSent = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please try again.");
+        }
+
+        if (isMessageSent)
+        {
+            Console.WriteLine("Message You've Unsent:");
+            Console.WriteLine(chatMessage);
+        }
+
+        Console.ReadLine();
+    }
+}
