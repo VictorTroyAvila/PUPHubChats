75de0469c4d1f824f526dfdf593e4cfee3632ab4
JohnChristopherRemolacio17 <116428397+JohnChristopherRemolacio17@users.noreply.github.com>
2023-06-24 00:03:55 +0800

    enable and disable

---
+++ Features_3/enable and disable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class ChatSystem
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the chat!");
+
+        bool chatEnabled = true;
+
+        while (true)
+        {
+            Console.Write("Enter your message: ");
+            string message = Console.ReadLine();
+
+            if (chatEnabled)
+            {
+                Console.WriteLine("Message sent: " + message);
+            }
+            else
+            {
+                Console.WriteLine("Chat is currently disabled. Your message cannot be sent.");
+            }
+
+            Console.WriteLine();
+
+            if (message.ToLower() == "disable")
+            {
+                chatEnabled = false;
+                Console.WriteLine("Chat disabled.");
+            }
+            else if (message.ToLower() == "enable")
+            {
+                chatEnabled = true;
+                Console.WriteLine("Chat enabled.");
+            }
+            else if (message.ToLower() == "exit")
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Chat session ended.");
+    }
+}
