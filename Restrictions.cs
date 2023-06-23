b4703d6d078b323227d2607e8de2b896d3f00de6
JohnChristopherRemolacio17 <116428397+JohnChristopherRemolacio17@users.noreply.github.com>
2023-06-24 00:48:35 +0800

    Transferring of codes

---
+++ Restrictions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class ChatRestriction
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the chat!");
+
+        bool isRestricted = false;
+        string[] restrictedWords = { "bad words", "inappropriate", "offensive" };
+
+        while (true)
+        {
+            Console.Write("Enter your message: ");
+            string message = Console.ReadLine();
+
+            if (isRestricted)
+            {
+                Console.WriteLine("Your message cannot be sent as it contains restricted words.");
+            }
+            else
+            {
+                foreach (string word in restrictedWords)
+                {
+                    if (message.ToLower().Contains(word.ToLower()))
+                    {
+                        isRestricted = true;
+                        break;
+                    }
+                }
+
+                if (isRestricted)
+                {
+                    Console.WriteLine("Your message cannot be sent as it contains restricted words.");
+                }
+                else
+                {
+                    Console.WriteLine("Message sent: " + message);
+                }
+            }
+
+            Console.WriteLine();
+
+            if (message.ToLower() == "exit")
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Chat session ended.");
+    }
+}
