6b102e3ec1685e88c0ddbdcd7364985919c18b45
Maria Yuki Villanueva <116412167+mariayukiv@users.noreply.github.com>
2023-06-30 16:04:52 +0800

    update

---
+++ GroupChat/Primary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GroupChat
+{
+	public class Primary
+	{
+		static void Main(string[] args)
+		{
+			GroupChatData groupChatData = new GroupChatData();
+			groupChatData.Run();
+		}
+	}
+}
