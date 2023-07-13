3cfee5407a78cccaeebe4c1ff227d4dfdd454955
VictorTroyAvila <116412427+VictorTroyAvila@users.noreply.github.com>
2023-07-13 19:39:07 +0800

    Integration of database
    I tried pero hanggang dyan lang kinaya hayst :(

---
+++ PUPHubChatData/SQLData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUPHubChatsData
+{
+    public class Class1
+    {
+        static string connectionString
+            = "Data Source =localhost; Initial Catalog = PUPHubPoints; Integrated Security = True;";
+        static SqlConnection sqlConnection;
+    }
+}
