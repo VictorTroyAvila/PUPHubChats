8e0b2dd4c068f3727f6b44ab5ab37a0acfc6dfc9
Maria Yuki Villanueva <mariayukigvillanueva@iskolarngbayan.pup.edu.ph>
2023-07-14 22:53:37 +0800

    added something and updated it

--- GroupChat/GroupChatData.cs
+++ GroupChat/GroupChatData.cs
@@ -3,51 +3,55 @@ using System.Collections.Generic;
 
 namespace GroupChat
 {
-	public class GroupChatData
-	{
-		private Dictionary<string, List<string>> groups = new Dictionary<string, List<string>>();
-		private GroupChatRules rules = new GroupChatRules();
 
-		public void Run()
-		{
-			Console.WriteLine("Welcome to our Group Chat System!");
+    public class GroupChatData
+    {
+        private Dictionary<string, List<string>> groups = new Dictionary<string, List<string>>();
+        private GroupChatRules rules = new GroupChatRules();
 
-			while (true)
-			{
-				Console.WriteLine("\n1. Join Group");
-				Console.WriteLine("2. Show Members in Group");
-				Console.WriteLine("3. Show Groups Joined");
-				Console.WriteLine("4. Create Group");
-				Console.WriteLine("5. View Groups");
-				Console.WriteLine("6. Exit");
-				Console.Write("\nEnter your choice: ");
-				int choice = Convert.ToInt32(Console.ReadLine());
+        public void Run(string role)
+        {
+            Console.WriteLine("Welcome to our Group Chat System!");
 
-				switch (choice)
-				{
-					case 1:
-						rules.JoinGroup(groups);
-						break;
-					case 2:
-						rules.ShowGroupMembers(groups);
-						break;
-					case 3:
-						rules.ShowJoinedGroups(groups);
-						break;
-					case 4:
-						rules.CreateGroup(groups);
-						break;
-					case 5:
-						rules.ViewGroups(groups);
-						break;
-					case 6:
-						Console.WriteLine("Exiting the program...");
-						return;
-					default:
-						Console.WriteLine("Invalid choice. Please try again.");
-						break;
-				}
-			}
-		}
-	}
+            while (true)
+            {
+                Console.WriteLine("\n1. Join Group");
+                Console.WriteLine("2. Show Members in Group");
+                Console.WriteLine("3. Show Groups Joined");
+                Console.WriteLine("4. Create Group");
+                Console.WriteLine("5. View Groups");
+                Console.WriteLine("6. Exit");
+                Console.Write("\nEnter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        rules.JoinGroup(groups);
+                        break;
+                    case 2:
+                        rules.ShowGroupMembers(groups);
+                        break;
+                    case 3:
+                        rules.ShowJoinedGroups(groups);
+                        break;
+                    case 4:
+                        if (role == "professor")
+                            rules.CreateGroup(groups);
+                        else
+                            Console.WriteLine("Sorry, students are not allowed to create groups.");
+                        break;
+                    case 5:
+                        rules.ViewGroups(groups);
+                        break;
+                    case 6:
+                        Console.WriteLine("Exiting the program...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
 }
---
+++ GroupChat/GroupChatModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GroupChat
+{
+
+    public class GroupChatModel
+    {
+        private GroupChatData groupChatData = new GroupChatData();
+
+        public void AskUserRole()
+        {
+            Console.Write("Please enter whether you're a student or a professor: ");
+            string role = Console.ReadLine().ToLower();
+
+            if (role == "student" || role == "professor")
+            {
+                groupChatData.Run(role);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'student' or 'professor'.");
+            }
+        }
+    }
+}
