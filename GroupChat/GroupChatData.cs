ac96626703859d96e705fddc9917cd00678974ba
Maria Yuki Villanueva <mariayukigvillanueva@iskolarngbayan.pup.edu.ph>
2023-07-28 01:45:35 +0800

    update ko rin here

--- GroupChat/GroupChatData.cs
+++ GroupChat/GroupChatData.cs
@@ -1,53 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GroupChat
+public class GroupChatData
 {
-    public class GroupChatData
+    public Dictionary<string, List<string>> Groups { get; set; }
+
+    public GroupChatData()
     {
-        private Dictionary<string, List<string>> groups = new Dictionary<string, List<string>>();
-        private GroupChatUI rules = new GroupChatUI();
-
-        public void Run()
-        {
-            Console.WriteLine("----- GROUP CHAT -----");
-
-            while (true)
-            {
-                Console.WriteLine("\n1. Join Group");
-                Console.WriteLine("2. Show Members in Group");
-                Console.WriteLine("3. Show Groups Joined");
-                Console.WriteLine("4. Create Group");
-                Console.WriteLine("5. View Groups");
-                Console.WriteLine("6. Exit");
-                Console.Write("\nEnter your choice: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
-
-                switch (choice)
-                {
-                    case 1:
-                        rules.JoinGroup(groups);
-                        break;
-                    case 2:
-                        rules.ShowGroupMembers(groups);
-                        break;
-                    case 3:
-                        rules.ShowJoinedGroups(groups);
-                        break;
-                    case 4:
-                        rules.CreateGroup(groups);
-                        break;
-                    case 5:
-                        rules.ViewGroups(groups);
-                        break;
-                    case 6:
-                        Console.WriteLine("Exiting the program...");
-                        return;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
-            }
-        }
+        Groups = new Dictionary<string, List<string>>();
     }
 }
--- GroupChat/GroupChatRules.cs
+++ GroupChat/GroupChatRules.cs
@@ -1,107 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GroupChat
+public class GroupChatRules
 {
+    private GroupChatData data;
 
-    public class GroupChatUI
+    public GroupChatRules()
     {
-        public void JoinGroup(Dictionary<string, List<string>> groups)
-        {
-            Console.Write("\nEnter your username: ");
-            string username = Console.ReadLine();
-            Console.Write("Enter the name of the group you want to join: ");
-            string groupName = Console.ReadLine();
+        data = new GroupChatData();
+    }
 
-            if (groups.ContainsKey(groupName))
+    public void JoinGroup(string username, string groupName)
+    {
+        if (data.Groups.ContainsKey(groupName))
+        {
+            data.Groups[groupName].Add(username);
+            Console.WriteLine($"You have joined the group: {groupName}.");
+        }
+        else
+        {
+            Console.WriteLine($"The group {groupName} does not exist.");
+        }
+    }
+
+    public void ShowGroupMembers(string groupName)
+    {
+        if (data.Groups.ContainsKey(groupName))
+        {
+            Console.WriteLine($"\nMembers of the group {groupName}:");
+            foreach (var member in data.Groups[groupName])
             {
-                groups[groupName].Add(username);
-                Console.WriteLine($"You have joined the group: {groupName}.");
+                Console.WriteLine(member);
             }
-            else
+        }
+        else
+        {
+            Console.WriteLine("The group does not exist.");
+        }
+    }
+
+    public void ShowJoinedGroups(string username)
+    {
+        Console.WriteLine($"\nGroups joined by {username}:");
+        bool foundGroup = false;
+        foreach (var group in data.Groups.Keys)
+        {
+            if (data.Groups[group].Contains(username))
             {
-                Console.WriteLine($"The group {groupName} does not exist.");
+                Console.WriteLine(group);
+                foundGroup = true;
             }
         }
 
-        public void ShowGroupMembers(Dictionary<string, List<string>> groups)
+        if (!foundGroup)
         {
-            Console.Write("\nEnter the name of the group: ");
-            string groupName = Console.ReadLine();
+            Console.WriteLine("You haven't joined any groups.");
+        }
+    }
 
-            if (groups.ContainsKey(groupName))
+    public void CreateGroup(string username, string groupName)
+    {
+        if (data.Groups.ContainsKey(groupName))
+        {
+            Console.WriteLine($"The group {groupName} already exists.");
+        }
+        else
+        {
+            data.Groups[groupName] = new List<string> { username };
+            Console.WriteLine($"Group {groupName} created successfully.");
+        }
+    }
+
+    public void ViewGroups()
+    {
+        Console.WriteLine("\nExisting Groups:");
+
+        if (data.Groups.Count > 0)
+        {
+            foreach (var group in data.Groups.Keys)
             {
-                Console.WriteLine($"\nMembers of the group {groupName}:");
-                foreach (var member in groups[groupName])
-                {
-                    Console.WriteLine(member);
-                }
-            }
-            else
-            {
-                Console.WriteLine("The group does not exist.");
+                Console.WriteLine(group);
             }
         }
-
-        public void ShowJoinedGroups(Dictionary<string, List<string>> groups)
+        else
         {
-            Console.Write("\nEnter your username: ");
-            string username = Console.ReadLine();
-
-            Console.WriteLine($"\nGroups joined by {username}:");
-            bool foundGroup = false;
-            foreach (var group in groups.Keys)
-            {
-                if (groups[group].Contains(username))
-                {
-                    Console.WriteLine(group);
-                    foundGroup = true;
-                }
-            }
-
-            if (!foundGroup)
-            {
-                Console.WriteLine("You haven't joined any groups.");
-            }
-        }
-
-        public void CreateGroup(Dictionary<string, List<string>> groups)
-        {
-            Console.Write("\nEnter your username: ");
-            string username = Console.ReadLine();
-            Console.Write("Enter the name of the group you want to create: ");
-            string groupName = Console.ReadLine();
-
-            if (groups.ContainsKey(groupName))
-            {
-                Console.WriteLine($"The group {groupName} already exists.");
-            }
-            else
-            {
-                groups[groupName] = new List<string> { username };
-                Console.WriteLine($"Group {groupName} created successfully.");
-            }
-        }
-
-        public void ViewGroups(Dictionary<string, List<string>> groups)
-        {
-            Console.WriteLine("\nExisting Groups:");
-
-            if (groups.Count > 0)
-            {
-                foreach (var group in groups.Keys)
-                {
-                    Console.WriteLine(group);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No groups available.");
-            }
+            Console.WriteLine("No groups available.");
         }
     }
 }
-
-
-
-
