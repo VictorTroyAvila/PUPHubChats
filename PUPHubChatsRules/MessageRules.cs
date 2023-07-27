0900f4b9887ddee74735411bc0c636cb6d7f3a68
DhukeMarquez <116818891+DhukeMarquez@users.noreply.github.com>
2023-07-27 23:36:46 +0800

    s

--- PUPHubChatsRules/MessageRules.cs
+++ PUPHubChatsRules/MessageRules.cs
@@ -9,17 +9,14 @@ namespace Feature1
         {
             if (userInput == "yes")
             {
-                Console.WriteLine("Message Has Been Unsent");
                 return true;
             }
             else if (userInput == "no")
             {
-                Console.WriteLine("Message Has Been Sent");
                 return false;
             }
             else
             {
-                Console.WriteLine("Invalid input. Please try again.");
                 return false;
             }
         }
