6483459e6453abdcdcc370434433d6803a6e990e
DhukeMarquez <116818891+DhukeMarquez@users.noreply.github.com>
2023-07-28 16:26:14 +0800

    s
    s

---
+++ PUPHubChatsData/StatusData.cs
@@ -0,0 +1,9 @@
+﻿class StatusData
+{
+    public string Online { get; } = "online";
+    public string TakingAnExam { get; } = "examination";
+    public string InMarketPlace { get; } = "marketplace";
+    public string Playing { get; } = "playing";
+    public string Studying { get; } = "study";
+    public string Offline { get; } = "offline";
+}
